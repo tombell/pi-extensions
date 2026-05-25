1cb0ee6829be67045b8ac57818567e39fa0a53ef
Tom Bell <tomb@tombell.dev>
2026-05-25 18:57:44 +0100

    Add oxfmt configuration

---
+++ oxfmt.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+  ignorePatterns: [],
+  sortImports: {
+    newlinesBetween: true,
+    groups: [
+      "type-import",
+      ["value-builtin", "value-external"],
+      "type-internal",
+      "value-internal",
+      ["type-parent", "type-sibling", "type-index"],
+      ["value-parent", "value-sibling", "value-index"],
+      "unknown",
+    ],
+  },
+});

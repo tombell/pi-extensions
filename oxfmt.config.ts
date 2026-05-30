95a2ef538b0f204edaf7abcb075793c8c9be840d
Tom Bell <tomb@tombell.dev>
2026-05-30 10:07:04 +0100

    Update oxfmt configuration

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -2,23 +2,5 @@ import { defineConfig } from "oxfmt";
 
 export default defineConfig({
   ignorePatterns: [],
-  sortImports: {
-    customGroups: [
-      {
-        groupName: "node",
-        elementNamePattern: ["node:*"],
-      },
-    ],
-    newlinesBetween: true,
-    groups: [
-      "node",
-      "type-import",
-      ["value-builtin", "value-external"],
-      "type-internal",
-      "value-internal",
-      ["type-parent", "type-sibling", "type-index"],
-      ["value-parent", "value-sibling", "value-index"],
-      "unknown",
-    ],
-  },
+  sortImports: true,
 });
